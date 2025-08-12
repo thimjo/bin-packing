6a99cfd2c26389f04bb8d15f5c01f317128ec79f
thimjo <thimjo.koca@gmail.com>
2025-08-12 22:13:29 +0200

    Add the problem-structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Problem {
+    bin_capacity: u32,
+    items_size: Vec<u32>,
+}
+
+impl Problem {
+    fn new(bin_capacity: u32, items_size: Vec<u32>) -> Problem {
+
+        Problem {
+            bin_capacity, items_size,
+        }
+    }
+
+    fn read_from_file(path: &str) -> Problem {
+        todo!("Check the standard file-formats and implement the method")
+    }
+}
